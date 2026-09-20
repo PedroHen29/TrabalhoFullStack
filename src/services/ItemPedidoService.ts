0776a1a36c407f8d140a1c64521af4455bbc332e
Pedro Henrique <pedroeichamaral@gmail.com>
2026-09-20 20:35:56 -0300

    Trabalho FullStack

--- src/services/ItemPedidoService.ts
+++ src/services/ItemPedidoService.ts
@@ -7,27 +7,51 @@ import { produtoRepository } from "../repository/produtoRepository";
 export class ItemPedidoService {
   async adicionarItem(pedidoId: number, dados: CriarPedidoDTO) {
     const pedido = await pedidoRepository.buscarPeloId(pedidoId);
-    if (!pedido) throw new NotFoundError("Pedido não encontrado");
+
+    if (!pedido) {
+      throw new NotFoundError("Pedido não encontrado");
+    }
 
     let valorTotalCalculado = Number(pedido.valorTotal) || 0;
 
     for (const item of dados.itens) {
       const produto = await produtoRepository.buscarPeloId(item.produtoId);
-      if (!produto) throw new NotFoundError("Produto não encontrado");
-      if (item.quantidade > produto.estoque)
+
+      if (!produto) {
+        throw new NotFoundError("Produto não encontrado");
+      }
+
+      if (item.quantidade > produto.estoque) {
         throw new BadRequestError("Quantidade maior que estoque");
+      }
+
+      const itemExistente = pedido.itens.find(
+        (itemPedido) => itemPedido.produto.id === item.produtoId,
+      );
+
+      if (itemExistente) {
+        itemExistente.quantidade += item.quantidade;
+
+        await itemPedidoRepository.salvar(itemExistente);
+      } else {
+        await itemPedidoRepository.criar({
+          quantidade: item.quantidade,
+          precoUnitario: Number(produto.preco),
+          pedido,
+          produto,
+        });
+      }
+
       produto.estoque -= item.quantidade;
       await produtoRepository.salvar(produto);
-      await itemPedidoRepository.criar({
-        quantidade: item.quantidade,
-        precoUnitario: Number(produto.preco),
-        pedido,
-        produto,
-      });
+
       valorTotalCalculado += Number(produto.preco) * item.quantidade;
     }
-    pedido.valorTotal = valorTotalCalculado
+
+    pedido.valorTotal = valorTotalCalculado;
+
     await pedidoRepository.salvar(pedido);
+
     return pedido;
   }
 
